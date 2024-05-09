7fc414dae42defa3dc65f63a336a81c31a708273
Alex Johnson <johnsonav1992@gmail.com>
2024-05-09 05:43:45 -0500

    added

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,17 @@
+use actix_web::{main, App, HttpServer};
+
 fn main() {
     println!("Backend is gonna be lit!!!! #rustftw");
+
+    #[main]
+    async fn main() -> std::io::Result<()> {
+        let port = 8080;
+        println!("Backend is gonna be lit!!!! #rustftw. Server running on port {}", port);
+
+        HttpServer::new(move || App::new())
+            .bind(("127.0.0.1", port))?
+            .workers(2)
+            .run()
+            .await
+}
 }

4048adc7693d4f7da0354580a4bf189b6017389c
Alex Johnson <johnsonav1992@gmail.com>
2024-05-09 05:45:41 -0500

    remove

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,15 +1,15 @@
 use actix_web::{main, App, HttpServer};
 
-fn main() {
-    #[main]
-    async fn main() -> std::io::Result<()> {
-        let port = 8080;
-        println!("Backend is gonna be lit!!!! #rustftw. Server running on port {}", port);
 
-        HttpServer::new(move || App::new())
-            .bind(("127.0.0.1", port))?
-            .workers(2)
-            .run()
-            .await
-    }   
+#[main]
+async fn main() -> std::io::Result<()> {
+    let port = 8080;
+    println!("Backend is gonna be lit!!!! #rustftw. Server running on port {}", port);
+
+    HttpServer::new(move || App::new())
+        .bind(("127.0.0.1", port))?
+        .workers(2)
+        .run()
+        .await
 }
+

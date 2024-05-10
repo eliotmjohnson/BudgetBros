967a5b536138d7d229f1cb523b7500a6477503d1
Alex Johnson <johnsonav1992@gmail.com>
2024-05-10 05:56:18 -0500

    db creds and connection

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -10,19 +10,23 @@ pub struct AppState {
 #[main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
-    // let db_url = std::env::var("DB_URL").expect("DB_URL must be set"); <- add in when we have a db url
-    // let pool = sqlx::postgres::PgPoolOptions::new()
-    //     .max_connections(5)
-    //     .connect(&db_url)
-    //     .await
-    //     .expect("Failed to connect to database"); <- add in when we have a db url
+    let db_url = std::env::var("DB_URL")
+        .expect("DB_URL env var must be set"); 
+
+    let pool = sqlx::postgres::PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&db_url)
+        .await
+        .expect("Failed to connect to database"); 
+
+    println!("Dat Cockroach DB is connected, yo!");
 
     let port = 8080;
     println!("Backend is gonna be lit!!!! #rustftw. Server running on port {}", port);
 
     HttpServer::new(move || {
         App::new()
-            // .app_data(Data::new( AppState { db: pool.clone() })) <- add this in when we have a db url
+            .app_data(Data::new( AppState { db: pool.clone() }))
     })
         .bind(("127.0.0.1", port))?
         .workers(2)

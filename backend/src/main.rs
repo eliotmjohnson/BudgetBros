0f604aef1a09299655315fbaca0fc237ec15be78
Alex Johnson <97169070+johnsonav1992@users.noreply.github.com>
2024-05-10 15:39:17 -0500

    Aj/db-creds (#5)

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,6 +1,6 @@
 use actix_web::{main, web::Data, App, HttpServer};
 use dotenv::dotenv;
-use sqlx::{Pool, Postgres};
+use sqlx::{Pool, Postgres, postgres::PgPoolOptions};
 
 pub struct AppState {
     db: Pool<Postgres>
@@ -9,20 +9,25 @@ pub struct AppState {
 
 #[main]
 async fn main() -> std::io::Result<()> {
-    dotenv().ok();
-    // let db_url = std::env::var("DB_URL").expect("DB_URL must be set"); <- add in when we have a db url
-    // let pool = sqlx::postgres::PgPoolOptions::new()
-    //     .max_connections(5)
-    //     .connect(&db_url)
-    //     .await
-    //     .expect("Failed to connect to database"); <- add in when we have a db url
-
     let port = 8080;
+    dotenv().ok();
+
+    let db_url = std::env::var("DB_URL")
+        .expect("DB_URL env var must be set"); 
+
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&db_url)
+        .await
+        .expect("Failed to connect to database"); 
+
+    println!("Dat Cockroach DB is connected, yo!");
+
     println!("Backend is gonna be lit!!!! #rustftw. Server running on port {}", port);
 
     HttpServer::new(move || {
         App::new()
-            // .app_data(Data::new( AppState { db: pool.clone() })) <- add this in when we have a db url
+            .app_data(Data::new( AppState { db: pool.clone() }))
     })
         .bind(("127.0.0.1", port))?
         .workers(2)

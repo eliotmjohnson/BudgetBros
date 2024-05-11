59757c1defafd1f4088aa6c66f933dca9c830065
Eliot Johnson <128415859+eliotmjohnson@users.noreply.github.com>
2024-05-10 19:26:53 -0500

    UI Additions (#8)

---
+++ src/app/components/main-window/main-window.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'MainWindow',
+    templateUrl: './main-window.component.html',
+    styleUrls: ['./main-window.component.scss']
+})
+export class MainWindowComponent {}

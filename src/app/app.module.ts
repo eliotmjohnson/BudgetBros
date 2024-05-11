d0b316c5f1b440d39e79e6ccf961f485b12e3c3c
Eliot Johnson <eliotmjohnson@gmail.com>
2024-05-10 19:26:02 -0500

    Adding some ui touches

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { LinkButtonComponent } from './components/link-button/link-button.component';
+import { MainWindowComponent } from './components/main-window/main-window.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         HeaderComponent,
         SidebarComponent,
-        LinkButtonComponent
+        LinkButtonComponent,
+        MainWindowComponent
     ],
     imports: [
         BrowserModule,

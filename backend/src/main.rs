aa4571bce50d362aba6153c74b0155437d94b88a
Alex Johnson <97169070+johnsonav1992@users.noreply.github.com>
2024-05-11 13:27:34 -0500

    Aj/auth-backend (#11)

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,15 +1,50 @@
-use actix_web::{main, web::Data, App, HttpServer};
+use actix_web::{dev::ServiceRequest, error::Error, main, web::{self, Data}, App, HttpMessage, HttpServer, Result};
+use actix_web_httpauth::{extractors::{
+        bearer::{self, BearerAuth}, 
+        AuthenticationError
+    }, middleware::HttpAuthentication};
 use dotenv::dotenv;
+use hmac::{Mac, Hmac};
+use jwt::VerifyWithKey;
+use serde::{Deserialize, Serialize};
+use sha2::Sha256;
 use sqlx::{Pool, Postgres, postgres::PgPoolOptions};
-use std::io::Result;
 
 pub struct AppState {
     db: Pool<Postgres>
 }
 
+// Will move this into another module soon
+#[derive(Serialize, Deserialize, Clone)]
+pub struct TokenClaims {
+    id: i32,
+}
+
+// Will move this into another module soon
+async fn token_validator(req: ServiceRequest, credentials: BearerAuth) -> Result<ServiceRequest, (Error, ServiceRequest)> {
+    let jwt_secret = std::env::var("JWT_SECRET").expect("JWT secret not found");
+    let key: Hmac<Sha256> = Hmac::new_from_slice(jwt_secret.as_bytes()).unwrap();
+    let token_string = credentials.token();
+
+    let claims: Result<TokenClaims, &str> = token_string
+        .verify_with_key(&key)
+        .map_err(|_| "Invalid token");
+
+    match claims {
+        Ok(value) => {
+            req.extensions_mut().insert(value);
+            Ok(req)
+        }
+        Err(_) => {
+            let config = req.app_data::<bearer::Config>().cloned().unwrap_or_default().scope("");
+
+            Err((AuthenticationError::from(config).into(), req))
+        }
+    }
+}
 
 #[main]
-async fn main() -> Result<()> {
+async fn main() -> std::io::Result<()> {
     let port = 8080;
     dotenv().ok();
 
@@ -27,8 +62,12 @@ async fn main() -> Result<()> {
     println!("Backend is gonna be lit!!!! #rustftw!!. Server running on port {}", port);
 
     HttpServer::new(move || {
+        let bearer_middleware = HttpAuthentication::bearer(token_validator);
+
         App::new()
             .app_data(Data::new( AppState { db: pool.clone() } ))
+            // .service(login) <- add back in when auth route done
+            // .service(register_user) <- add in when route ready
     })
         .bind(("127.0.0.1", port))?
         .workers(2)

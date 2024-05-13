db7d71f764fe5f1a9009adcbb13e6f03c7cef5b8
Eliot Johnson <eliotmjohnson@gmail.com>
2024-05-13 09:15:53 -0500

    Added menu dropdown

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { LinkButtonComponent } from './components/link-button/link-button.component';
 import { MainWindowComponent } from './components/main-window/main-window.component';
+import { ContentCardComponent } from './components/content-card/content-card.component';
 
 @NgModule({
     declarations: [
@@ -18,14 +21,17 @@ import { MainWindowComponent } from './components/main-window/main-window.compon
         HeaderComponent,
         SidebarComponent,
         LinkButtonComponent,
-        MainWindowComponent
+        MainWindowComponent,
+        ContentCardComponent
     ],
     imports: [
         BrowserModule,
         AppRoutingModule,
         BrowserAnimationsModule,
         MatIconModule,
-        MatButtonModule
+        MatButtonModule,
+        MatCardModule,
+        MatMenuModule
     ],
     providers: [],
     bootstrap: [AppComponent]

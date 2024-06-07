d2eead84e103fc66dcebe2ebf84c05fac246bd05
Eliot Johnson <128415859+eliotmjohnson@users.noreply.github.com>
2024-06-07 15:41:05 -0500

    Angular 18 Upgrade (#31)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import {
+    provideHttpClient,
+    withInterceptorsFromDi
+} from '@angular/common/http';
 
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
@@ -51,11 +54,11 @@ import { BudgetCategoryItemComponent } from './components/budget-category-item/b
         BudgetCategoryCardComponent,
         BudgetCategoryItemComponent
     ],
+    bootstrap: [AppComponent],
     imports: [
         BrowserModule,
         AppRoutingModule,
         BrowserAnimationsModule,
-        HttpClientModule,
         MatIconModule,
         MatButtonModule,
         MatCardModule,
@@ -66,7 +69,6 @@ import { BudgetCategoryItemComponent } from './components/budget-category-item/b
         MatProgressSpinnerModule,
         DragDropModule
     ],
-    providers: [],
-    bootstrap: [AppComponent]
+    providers: [provideHttpClient(withInterceptorsFromDi())]
 })
 export class AppModule {}

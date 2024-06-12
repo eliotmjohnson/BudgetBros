f1f8c2236d994ddd336f36b3a21d0704129439b8
Eliot Johnson <eliotmjohnson@gmail.com>
2024-06-11 19:46:43 -0500

    Init commit

---
+++ src/app/components/transactions-card/transactions-card.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'TransactionsCard',
+    templateUrl: './transactions-card.component.html',
+    styleUrl: './transactions-card.component.scss'
+})
+export class TransactionsCardComponent {}

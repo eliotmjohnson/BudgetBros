d97909b43cf1ec6638146640cf0e2e4fe3967b04
Alex Johnson <johnsonav1992@gmail.com>
2024-06-29 14:02:40 -0500

    add migration

--- backend/migrations/0003_transactions.sql
+++ backend/migrations/0003_transactions.sql
@@ -1,12 +1,12 @@
--- CREATE TABLE transactions (
---     id SERIAL PRIMARY KEY,
---     title VARCHAR NOT NULL,
---     merchant VARCHAR NOT NULL,
---     amount FLOAT8 NOT NULL,
---     notes TEXT,
---     date TIMESTAMPTZ NOT NULL,
---     line_item_id INTEGER,
---     CONSTRAINT fk_line_item
---         FOREIGN KEY(line_item_id)
---         REFERENCES line_items(id)
--- );
+CREATE TABLE transactions (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR NOT NULL,
+    merchant VARCHAR NOT NULL,
+    amount FLOAT8 NOT NULL,
+    notes TEXT,
+    date TIMESTAMPTZ NOT NULL
+    -- line_item_id INTEGER,
+    -- CONSTRAINT fk_line_item
+    --     FOREIGN KEY(line_item_id)
+    --     REFERENCES line_items(id)
+);

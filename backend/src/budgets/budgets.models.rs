b591b7cafb4b14687744f7f0c17ed4ab0cec4bf8
Eliot Johnson <eliotmjohnson@gmail.com>
2024-08-02 14:57:20 -0500

    Conflict fixes

--- backend/src/budgets/budgets.models.rs
+++ backend/src/budgets/budgets.models.rs
@@ -4,7 +4,7 @@ use chrono::{DateTime, Local};
 use serde::{Deserialize, Serialize};
 use sqlx::{self, FromRow};
 
-#[derive(Serialize, FromRow, Debug)]
+#[derive(Serialize, FromRow)]
 pub struct Budget {
     pub budget_id: i64,
     pub user_id: i64,
@@ -12,7 +12,8 @@ pub struct Budget {
     pub year: i64,
 }
 
-#[derive(Serialize, FromRow, Debug)]
+#[derive(Serialize, FromRow)]
+#[serde(rename_all = "camelCase")]
 pub struct BudgetRowData {
     pub budget_id: i64,
     pub user_id: i64,
@@ -55,7 +56,6 @@ pub struct LineItemData {
     pub transactions: Vec<TransactionData>,
 }
 
-#[derive(Serialize, Deserialize, Clone)]
 pub struct BudgetCategoryDataMap {
     pub budget_category_id: i64,
     pub name: String,
@@ -67,10 +67,10 @@ pub struct BudgetCategoryDataMap {
 pub struct BudgetCategoryDataConverted {
     pub budget_category_id: i64,
     pub name: String,
-    pub budget_line_items: Vec<LineItemData>,
+    pub line_items: Vec<LineItemData>,
 }
 
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct BudgetResponseData {
     pub budget_id: i64,

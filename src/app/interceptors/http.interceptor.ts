2ccb143d78362b913b46a090664ca98ed9543a5c
Alex Johnson <johnsonav1992@gmail.com>
2024-08-07 06:25:52 -0500

    interceptor

---
+++ src/app/interceptors/http.interceptor.ts
@@ -0,0 +1,17 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+
+export const httpInterceptor: HttpInterceptorFn = (req, next) => {
+  const token = localStorage.getItem('token');
+
+  if (token) {
+    const authReq = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+
+    return next(authReq);
+  }
+
+  return next(req);
+};

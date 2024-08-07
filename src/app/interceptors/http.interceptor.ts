fbfbb722f2b8605a3db5824048d1091bec23c636
Alex Johnson <johnsonav1992@gmail.com>
2024-08-07 06:29:52 -0500

    don't include unprotected routes

--- src/app/interceptors/http.interceptor.ts
+++ src/app/interceptors/http.interceptor.ts
@@ -2,8 +2,11 @@ import { HttpInterceptorFn } from '@angular/common/http';
 
 export const httpInterceptor: HttpInterceptorFn = (req, next) => {
   const token = localStorage.getItem('token');
+  const unprotectedRoutes = ['login', 'session-refresh', 'register']
 
-  if (token) {
+  const isUnprotectedRoute = unprotectedRoutes.some(route => req.url.includes(route));
+
+  if (token && !isUnprotectedRoute) {
     const authReq = req.clone({
       setHeaders: {
         Authorization: `Bearer ${token}`

1461d806abcc30655193fc68248371d702f7520d
Eliot Johnson <128415859+eliotmjohnson@users.noreply.github.com>
2024-08-08 22:23:55 -0500

    Refactored Http Interceptor (#59)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {
-    provideHttpClient,
-    withInterceptors
-} from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';

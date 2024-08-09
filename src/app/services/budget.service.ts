2004ead02aa3236a383dc813c19c968af8b72932
Eliot Johnson <eliotmjohnson@gmail.com>
2024-08-08 20:50:05 -0500

    Added budget service changes

--- src/app/services/budget.service.ts
+++ src/app/services/budget.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject, signal } from '@angular/core';
 import { BASE_BE_URL } from '../constants/constants';
-import { toSignal } from '@angular/core/rxjs-interop';
 import { Budget } from '../models/budget';
 import { AuthService } from './auth.service';
 
@@ -14,15 +13,26 @@ export class BudgetService {
     http = inject(HttpClient);
     authService = inject(AuthService);
 
+    budget = signal<Budget | undefined>(undefined);
+    isLoading = signal(false);
+    getBudgetError = signal<unknown>(null);
+
     getBudget(monthNumber: number, year: number) {
         if (this.authService.userId) {
-            return toSignal(
-                this.http.get<Budget>(
+            this.isLoading.set(true);
+            this.http
+                .get<Budget>(
                     `${baseUrl}/${this.authService.userId}?month_number=${monthNumber}&year=${year}`
                 )
-            );
+                .subscribe({
+                    next: (budget) => {
+                        this.isLoading.set(false);
+                        this.budget.set(budget);
+                    },
+                    error: (error) => {
+                        this.getBudgetError.set(error);
+                    }
+                });
         }
-
-        return signal(undefined);
     }
 }

d76060d6f6b99c4b31205d285d4a6718d5d744bb
Eliot Johnson <eliotmjohnson@gmail.com>
2024-08-24 16:06:42 -0500

    Merge conflicts and Formatting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,53 +1,53 @@
 import { NgModule } from '@angular/core';
 
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
 
-import { MatChipsModule } from '@angular/material/chips';
-import { MatIconModule } from '@angular/material/icon';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatChipsModule } from '@angular/material/chips';
 import {
     MatDateRangeInput,
     MatDateRangePicker,
     MatDatepickerModule,
     MatDatepickerToggle
 } from '@angular/material/datepicker';
-import { MatDividerModule } from '@angular/material/divider';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTooltipModule } from '@angular/material/tooltip';
 
-import { AppComponent } from './app.component';
-import { HomeHeaderComponent } from './components/home-header/home-header.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { LinkButtonComponent } from './components/link-button/link-button.component';
-import { HomeComponent } from './pages/home/home.component';
-import { LoginComponent } from './pages/login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { AddTransactionModalComponent } from './components/add-transaction-modal/add-transaction-modal.component';
 import { AuthFormComponent } from './components/auth-form/auth-form.component';
 import { BBLogoComponent } from './components/bb-logo/bb-logo.component';
-import { BudgetComponent } from './pages/home/budget/budget.component';
-import { TransactionsComponent } from './pages/home/transactions/transactions.component';
-import { GoalsComponent } from './pages/home/goals/goals.component';
-import { AccountsComponent } from './pages/home/accounts/accounts.component';
-import { SettingsComponent } from './pages/home/settings/settings.component';
-import { CardComponent } from './components/card/card.component';
 import { BudgetCategoryCardComponent } from './components/budget-category-card/budget-category-card.component';
 import { BudgetCategoryItemComponent } from './components/budget-category-item/budget-category-item.component';
-import { TransactionCardComponent } from './components/transaction-card/transaction-card.component';
 import { BudgetTransactionsCardComponent } from './components/budget-transactions-card/budget-transactions-card.component';
-import { httpInterceptor } from './interceptors/http.interceptor';
-import { TransactionsDatePickerComponent } from './components/transactions-date-picker/transactions-date-picker.component';
+import { CardComponent } from './components/card/card.component';
+import { HomeHeaderComponent } from './components/home-header/home-header.component';
 import { InlineTransactionComponent } from './components/inline-transaction/inline-transaction.component';
-import { AddTransactionModalComponent } from './components/add-transaction-modal/add-transaction-modal.component';
-import { provideNativeDateAdapter } from '@angular/material/core';
+import { LinkButtonComponent } from './components/link-button/link-button.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { TransactionCardComponent } from './components/transaction-card/transaction-card.component';
+import { TransactionsDatePickerComponent } from './components/transactions-date-picker/transactions-date-picker.component';
+import { httpInterceptor } from './interceptors/http.interceptor';
+import { AccountsComponent } from './pages/home/accounts/accounts.component';
+import { BudgetComponent } from './pages/home/budget/budget.component';
+import { GoalsComponent } from './pages/home/goals/goals.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SettingsComponent } from './pages/home/settings/settings.component';
+import { TransactionsComponent } from './pages/home/transactions/transactions.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
     declarations: [

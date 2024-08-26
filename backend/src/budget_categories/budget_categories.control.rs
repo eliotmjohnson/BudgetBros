59563b6f03337769b85504b0f81bfbcdbb39228f
Alex Johnson <johnsonav1992@gmail.com>
2024-08-25 20:55:10 -0500

    need to fetch budget categories with line items

--- backend/src/budget_categories/budget_categories.control.rs
+++ backend/src/budget_categories/budget_categories.control.rs
@@ -1,20 +1,31 @@
 use actix_web::{
     get,
-    web::{Data, Path},
+    web::{Data, Path, Query},
     HttpResponse, Responder,
 };
 
-use crate::AppState;
+use crate::{budget_categories::budget_categories_services::get_budget_categories_with_line_items, AppState};
 
-use super::budget_categories_services::get_budget_categories;
+
+pub struct QueryParams {
+    date: Option<String>,
+}
 
 #[get("/{user_id}")]
 async fn get_all_budget_categories_handler(
     state: Data<AppState>,
     params: Path<i64>,
+    query: Query<QueryParams>
 ) -> impl Responder {
     let user_id = params.into_inner();
-    let budget_categories_result = get_budget_categories(state, user_id).await;
+    let date = query.date.clone().expect("Date query param is missing");
+
+    let budget_categories_result = 
+        get_budget_categories_with_line_items(
+            state, 
+            user_id, 
+            date
+        ).await;
 
     match budget_categories_result {
         Ok(budget_categories) => HttpResponse::Ok().json(budget_categories),
--- backend/src/budget_categories/budget_categories.service.rs
+++ backend/src/budget_categories/budget_categories.service.rs
@@ -4,15 +4,26 @@ use crate::AppState;
 
 use super::budget_categories_models::BudgetCategory;
 
-pub async fn get_budget_categories(
+pub async fn get_budget_categories_with_line_items(
     state: Data<AppState>,
     budget_id: i64,
+    date: String
 ) -> Result<Vec<BudgetCategory>, sqlx::Error> {
-    sqlx::query_as::<_, BudgetCategory>(
-        "SELECT * FROM budget_categories
-            WHERE budget_id = $1",
-    )
-    .bind(budget_id)
-    .fetch_all(&state.db)
-    .await
+    let query = "
+        SELECT *
+        FROM 
+            budget_categories
+        JOIN 
+            line_items ON budget_categories.line_item_id = line_items.id
+        WHERE 
+            budget_id = $1
+        AND 
+            date = $2
+    ";
+
+    sqlx::query_as::<_, BudgetCategory>(query)
+        .bind(budget_id)
+        .bind(date)
+        .fetch_all(&state.db)
+        .await
 }

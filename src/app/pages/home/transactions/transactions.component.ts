3cf528a0b03999d1d9b8d7dfdd285839103101d3
Eliot Johnson <128415859+eliotmjohnson@users.noreply.github.com>
2024-08-27 21:08:01 -0500

    Default Formatter (#77)

--- src/app/pages/home/transactions/transactions.component.ts
+++ src/app/pages/home/transactions/transactions.component.ts
@@ -1,50 +1,52 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
-import { TransactionModalComponent, TransactionModalData } from 'src/app/components/transaction-modal/transaction-modal.component';
-import { BudgetCategoryService } from 'src/app/services/budget-category.service';
+import {
+    TransactionModalComponent,
+    TransactionModalData
+} from 'src/app/components/transaction-modal/transaction-modal.component';
 import { TransactionService } from 'src/app/services/transaction.service';
 
 @Component({
-  selector: 'app-transactions',
-  templateUrl: './transactions.component.html',
-  styleUrls: ['./transactions.component.scss']
+    selector: 'app-transactions',
+    templateUrl: './transactions.component.html',
+    styleUrls: ['./transactions.component.scss']
 })
 export class TransactionsComponent implements OnInit {
-  transactionService = inject(TransactionService);
-  dialog = inject(MatDialog);
+    transactionService = inject(TransactionService);
+    dialog = inject(MatDialog);
 
-  form = new FormGroup({
-    start: new FormControl<Date | null>(new Date()),
-    end: new FormControl<Date | null>(new Date())
-  })
+    form = new FormGroup({
+        start: new FormControl<Date | null>(new Date()),
+        end: new FormControl<Date | null>(new Date())
+    });
 
-  transactions = this.transactionService.transactions;
+    transactions = this.transactionService.transactions;
 
-  ngOnInit(): void {
-    this.transactionService
-      .getTransactionsBetweenDates(new Date(), new Date())
-  }
-
-  openAddTransactionDialog() {
-    const dialogRef = this.dialog
-      .open<
-        TransactionModalComponent, 
-        TransactionModalData
-      >(TransactionModalComponent, {
-        data: {
-          mode: 'add'
-        }
-    })
-  }
-
-  submitForm() {
-    const start = this.form.get('start')?.value;
-    const end = this.form.get('end')?.value;
-
-    if (start && end) {
-      this.transactionService.getTransactionsBetweenDates(start, end);
+    ngOnInit(): void {
+        this.transactionService.getTransactionsBetweenDates(
+            new Date(),
+            new Date()
+        );
     }
-  }
 
+    openAddTransactionDialog() {
+        const dialogRef = this.dialog.open<
+            TransactionModalComponent,
+            TransactionModalData
+        >(TransactionModalComponent, {
+            data: {
+                mode: 'add'
+            }
+        });
+    }
+
+    submitForm() {
+        const start = this.form.get('start')?.value;
+        const end = this.form.get('end')?.value;
+
+        if (start && end) {
+            this.transactionService.getTransactionsBetweenDates(start, end);
+        }
+    }
 }

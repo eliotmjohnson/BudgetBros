74306709a5e619144e75c546b4fd7bdfcaecdc1f
Alex Johnson <johnsonav1992@gmail.com>
2024-09-11 05:47:35 -0500

    overlay is being weird

--- src/app/components/transactions-filter-popover/transactions-filter-popover.component.ts
+++ src/app/components/transactions-filter-popover/transactions-filter-popover.component.ts
@@ -1,12 +1,35 @@
 import { CdkOverlayOrigin } from '@angular/cdk/overlay';
-import { Component, input } from '@angular/core';
+import { Component, OnInit, input } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import {
+    addValueToCurrencyInput,
+    checkCurrencyInputKeyValid
+} from 'src/app/utils/currencyUtils';
 
 @Component({
     selector: 'TransactionsFilterPopover',
     templateUrl: './transactions-filter-popover.component.html',
     styleUrl: './transactions-filter-popover.component.scss'
 })
-export class TransactionsFilterPopoverComponent {
+export class TransactionsFilterPopoverComponent implements OnInit {
     trigger = input.required<CdkOverlayOrigin>();
     isFilterPopoverOpen = input.required<boolean>();
+
+    form = new FormGroup({
+        titleOrMerchant: new FormControl<string>(''),
+        date: new FormControl<Date | null>(null),
+        amount: new FormControl<number | null>(null)
+    });
+
+    ngOnInit(): void {
+        this.form.valueChanges.subscribe((value) => console.log(value));
+    }
+
+    checkIfValidKey(e: KeyboardEvent): boolean {
+        return checkCurrencyInputKeyValid(e, this.form.value.amount as number);
+    }
+
+    addValue(e: Event) {
+        addValueToCurrencyInput(e, this.form);
+    }
 }

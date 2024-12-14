0cec8095c4044e57c9b1da2d7b3307533ec0611f
Alex Johnson <johnsonav1992@gmail.com>
2024-12-13 19:48:18 -0600

    BBLogo

--- src/app/components/bb-logo/bb-logo.component.ts
+++ src/app/components/bb-logo/bb-logo.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
     selector: 'BBLogo',
     templateUrl: './bb-logo.component.html',
-    styleUrls: ['./bb-logo.component.scss']
+    styleUrls: ['./bb-logo.component.scss'],
+    imports: [MatIcon]
 })
 export class BBLogoComponent {
     @Input() fontSize = '';

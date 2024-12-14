64c493e62b5eb7e77c67bed60a438648a7215b6c
Alex Johnson <johnsonav1992@gmail.com>
2024-12-13 20:07:00 -0600

    home-header

--- src/app/components/home-header/home-header.component.ts
+++ src/app/components/home-header/home-header.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { BudgetService } from 'src/app/services/budget.service';
+import { BBLogoComponent } from '../bb-logo/bb-logo.component';
+import { MatMenu, MatMenuTrigger } from '@angular/material/menu';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
     selector: 'HomeHeader',
     templateUrl: './home-header.component.html',
-    styleUrls: ['./home-header.component.scss']
+    styleUrls: ['./home-header.component.scss'],
+    imports: [BBLogoComponent, MatMenuTrigger, MatIcon, MatMenu]
 })
 export class HomeHeaderComponent {
     constructor(

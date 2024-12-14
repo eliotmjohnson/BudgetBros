569e9cd5a931f28072b453b7c4fbdd8811c6a773
Alex Johnson <johnsonav1992@gmail.com>
2024-12-13 19:59:01 -0600

    budget starter

--- src/app/components/budget-starter/budget-starter.component.ts
+++ src/app/components/budget-starter/budget-starter.component.ts
@@ -1,11 +1,15 @@
 import { Component, inject, OnInit, output, signal } from '@angular/core';
 import { BudgetService } from 'src/app/services/budget.service';
 import { MobileModalService } from 'src/app/services/mobile-modal.service';
+import { BBLogoComponent } from '../bb-logo/bb-logo.component';
+import { MatSpinner } from '@angular/material/progress-spinner';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
     selector: 'BudgetStarter',
     templateUrl: './budget-starter.component.html',
-    styleUrl: './budget-starter.component.scss'
+    styleUrl: './budget-starter.component.scss',
+    imports: [BBLogoComponent, MatSpinner, MatIcon]
 })
 export class BudgetStarterComponent implements OnInit {
     budgetService = inject(BudgetService);

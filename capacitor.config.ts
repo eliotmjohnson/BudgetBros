13a73e6919afbe2fa5f30c88a2e1087771f054cd
Eliot Johnson <eliotmjohnson@gmail.com>
2025-07-14 17:21:41 -0500

    Remove cap server

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,11 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
     appId: 'com.budgetbros.app',
     appName: 'BudgetBros',
-    webDir: 'dist/budget-bros/browser',
-    server: {
-        url: 'http://192.168.1.64:4200',
-        cleartext: true
-    }
+    webDir: 'dist/budget-bros/browser'
 };
 
 export default config;
